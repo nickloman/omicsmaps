97e7ed2966fd3daa1ec23a0ed5b45e22c1deb373
Nick Loman <n.j.loman@bham.ac.uk>
2015-01-03 18:54:39 +0000

    staticfiles

--- htseq/settings.py
+++ htseq/settings.py
@@ -138,3 +138,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'htseq/static'),
 )
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
