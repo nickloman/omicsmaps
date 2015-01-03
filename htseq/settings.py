09e5cdd3812bc87a53a894c614bc8ca08385eba8
Nick Loman <n.j.loman@bham.ac.uk>
2015-01-03 16:39:28 +0000

    add settings

---
+++ htseq/settings.py
@@ -0,0 +1,138 @@
+# Django settings for htseq project.
+import sys
+import os
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+SEND_BROKEN_LINKS_EMAIL=True
+EMAIL_HOST='smtp.bham.ac.uk'
+EMAIL_PORT=25
+
+ADMINS = (
+    # ('Your Name', 'your_email@domain.com'),
+    ('Nick Loman', 'n.j.loman@bham.ac.uk'),
+)
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+ROOT_DIRECTORY = BASE_DIR
+
+MANAGERS = ADMINS
+
+DATABASE_ENGINE = 'sqlite3'    # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': ROOT_DIRECTORY + '/htseq/db/htseq.sqlite',
+#    }
+#}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'nick',
+        'USER': 'nick',
+        'HOST': ''
+    }
+}
+
+# Local time zone for this installation. Choices can be found here:
+# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
+# although not all choices may be available on all operating systems.
+# If running in a Windows environment this must be set to the same as your
+# system time zone.
+TIME_ZONE = 'Europe/London'
+
+# Language code for this installation. All choices can be found here:
+# http://www.i18nguy.com/unicode/language-identifiers.html
+LANGUAGE_CODE = 'en-us'
+
+SITE_ID = 1
+
+# If you set this to False, Django will make some optimizations so as not
+# to load the internationalization machinery.
+USE_I18N = True
+
+# Absolute path to the directory that holds media.
+# Example: "/home/media/media.lawrence.com/"
+MEDIA_ROOT = ''
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash if there is a path component (optional in other cases).
+# Examples: "http://media.lawrence.com", "http://example.com/media/"
+MEDIA_URL = ''
+
+# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
+# trailing slash.
+# Examples: "http://foo.com/media/", "/media/".
+ADMIN_MEDIA_PREFIX = '/hts/media/'
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = 'l2=k_vp5j02_n01of16lj2l-v(x7&1p891n=x$+o7vtms1)g@y'
+
+# List of callables that know how to import templates from various sources.
+MIDDLEWARE_CLASSES = (
+    'django.middleware.gzip.GZipMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'htseq.urls'
+
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    ROOT_DIRECTORY + '/htseq/templates',
+)
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.admin',
+    'htseq.centres',
+    'htseq.stats',
+    'htseq.serial',
+)
+
+GOOGLE_API_KEY = 'ABQIAAAAgGdBn7SxbtaVLrnyaw_DuRQjfajE9aXNLPmcFbHvJ1n2-MqamBS1tV-nD70aps4K1YDhzuSCg_X5Pg'
+
+STATIC_PATH = 'http://omicsmaps.com'
+
+STATIC_VERSION = 12
+
+FORCE_SCRIPT_NAME = ''
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': 'omicsmaps'
+    }
+}
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
